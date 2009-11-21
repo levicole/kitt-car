83f1b4ed8d49fa6db374981803d45228b3f115d1
LeviCole <lckennedy@gmail.com>
2009-11-20 22:35:48 -0600

    initial commit

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "main.h"
+int main ()
+{
+	//setup
+	// configure PB0 for output
+	pinMode(LED1, OUT);
+  pinMode(LED2, OUT);
+  pinMode(LED3, OUT);
+  pinMode(LED4, OUT);
+  pinMode(LED5, OUT);
+  pinMode(LED6, OUT);
+	while(1)
+	{
+		digitalWrite(LED1, HIGH);
+		_delay_ms(100);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, HIGH);
+		_delay_ms(100);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+		_delay_ms(100);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, HIGH);
+		_delay_ms(100);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, HIGH);
+		_delay_ms(100);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED6, HIGH);
+		_delay_ms(100);
+    digitalWrite(LED6, LOW);
+    digitalWrite(LED5, HIGH);
+		_delay_ms(100);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED4, HIGH);
+		_delay_ms(100);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED3, HIGH);
+		_delay_ms(100);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED2, HIGH);
+		_delay_ms(100);
+    digitalWrite(LED2, LOW);
+	}
+	return 0;
+}
+
+void pinMode(uint8_t pin, uint8_t mode)
+{
+	if(mode == OUT)	DDRB |= (1 << pin);
+	else DDRB &= ~(1 << pin);
+}
+
+void digitalWrite(uint8_t pin, uint8_t state)
+{
+	if(state == HIGH)	PORTB |= (1 << pin);
+	else PORTB &= ~(1 << pin);
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#define LED_BIT PB0
+#define IN 0
+#define OUT 1
+#define LOW 0
+#define HIGH 1
+void pinMode(uint8_t pin, uint8_t mode);
+void digitalWrite(uint8_t pin, uint8_t state);
